562c24f45cc7616e1c38872a3126bf94982ee895
xubin30 <xubin30@meituan.com>
2021-06-29 16:26:09 +0800

    first commit

--- background.js
+++ background.js
@@ -1,10 +1,25 @@
 chrome.contextMenus.create({
     type: 'normal',
-    title: '用谷歌翻译一下吧',
+    title: '查看字体',
     contexts: ['selection'],
     onclick: function(info) {
-        console.log(info)
-        const href = `https://translate.google.cn/?sl=en&tl=zh-CN&text=${info.selectionText}%0A&op=translate`
-        window.open(href)
+        // const href = `https://translate.google.cn/?sl=en&tl=zh-CN&text=${info.selectionText}%0A&op=translate`
+        // window.open(href)
+
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, (tabs) => {
+            let message = {
+                info: '点击了'
+            }
+            chrome.tabs.sendMessage(tabs[0].id, message, res => {
+                if (res.code === 0) {
+                    alert(res.msg || '')
+                } else {
+                    alert(res)
+                }
+            })
+        })
     } 
 })
---
+++ inject.js
@@ -0,0 +1,56 @@
+function getFontInfo () {
+    var anchorNode = window.getSelection().anchorNode
+
+    if (!anchorNode) {
+        return "请先选中字体！"
+    }
+
+    var parent = anchorNode.parentNode
+
+    if (!parent) {
+        return "选中错误！"
+    }
+
+    var attrMap = window.getComputedStyle(parent)
+
+    var attrs = [
+        {
+            key: 'color',
+            name: '颜色'
+        },
+        {
+            key: 'lineHeight',
+            name: '行高'
+        },
+        {
+            key: 'fontSize',
+            name: '字体大小'
+        },
+        {
+            key: 'fontFamily',
+            name: '字体类型'
+        }
+    ]
+
+    let msg = ''
+
+    attrs.forEach(el => {
+        const { key } = el
+        el.value = attrMap[key]
+        msg += el.name + el.value + '\n'
+    })
+
+    
+
+    return {
+        code: 0,
+        data: attrs,
+        msg: msg
+    }
+}
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // 这里是返回给bg的内容
+    sendResponse(getFontInfo())
+})
